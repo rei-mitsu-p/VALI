c5df9e538131a432572a3661079f2d3926233ebe
rei-mitsu-p <rei-mitsu-p>
2025-04-12 04:57:28 +0900

    【frontend】frontendモジュールを新規追加

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/vue3-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+    ecmaVersion: 2022,
+    sourceType: "module",
+  },
+  plugins: ["vue", "@typescript-eslint", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "vue/multi-word-component-names": "off",
+  },
+};
---
+++ frontend/src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import ninja from "./assets/ninja.svg";
+import InfoMsg from "./components/InfoMsg.vue";
+</script>
+
+<template>
+  <div class="center">
+    <a href="/">
+      <img src="/img/squid.svg" class="logo blue-shadow" />
+    </a>
+    <img :src="ninja" class="logo red-shadow" />
+    <InfoMsg msg="VALI TOP" />
+  </div>
+</template>
+
+<style scoped>
+.center {
+  text-align: center;
+}
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  transition: filter 300ms;
+}
+.logo.blue-shadow:hover {
+  filter: drop-shadow(0 0 2em blue);
+}
+.logo.red-shadow:hover {
+  filter: drop-shadow(0 0 2em red);
+}
+</style>
